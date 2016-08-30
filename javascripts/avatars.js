5671c6c919d312945e1922fdb6fee3ee1fb1f4a5
mbMosman <mbmosman@users.noreply.github.com>
2016-08-30 04:31:57 -0500

    first commit

---
+++ javascripts/avatars.js
@@ -0,0 +1,20 @@
+$(function() {
+  $('.student').each(function() {
+    var $student = $(this);
+    var username = $student.data('username');
+    var $avatar = $student.find('.avatar');
+
+    $.ajax({
+      url: 'https://api.github.com/users/' + username,
+      dataType: 'json',
+      timeout: 2000,
+      cache: true,
+      success: function(data) {
+        $avatar.attr('src', data.avatar_url);
+      },
+      error: function() {
+        $avatar.attr('src', 'https://github.com/identicons/' + username + '.png');
+      }
+    })
+  });
+});
